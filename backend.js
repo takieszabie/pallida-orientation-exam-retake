27324fe9eddd060b6dcb016d65850c2804bfd233
András Kamocsai <kamo.andras@gmail.com>
2017-11-27 13:39:58 +0100

    feat(backend.js): added two endpoints

--- backend.js
+++ backend.js
@@ -1,5 +1,3 @@
-'use strict';
-
 'use strict'
 
 const express = require('express');
@@ -14,7 +12,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root',
-    database: 'licence_plate'
+    database: 'clothing'
 });
 
 connection.connect(function (err){
@@ -29,27 +27,27 @@ app.get('/', function (req, res){
     res.sendFile(__dirname + '/assets/index.html');
 });
 
-app.get('/search', function(req, res){
-    let filter = '';
+app.get('/warehouse', function(req, res){
+    // let filter = '';
     
-    if( req.query.police ){  // query stringben átjövő adat
-        filter = 'RB%';
-    };
+    // if( req.query.police ){  
+    //     filter = 'RB%';
+    // };
     
-    if( req.query.diplomat ){
-        filter = 'DT%';
-    };
-    let sqlCode = `SELECT * FROM licence_plates WHERE plate LIKE "${filter}%${req.query.plate}%"`; // A ? jel helyére jön a [] közötti req.query.plate!!!
+    // if( req.query.diplomat ){
+    //     filter = 'DT%';
+    // };
+    let sqlCode = `SELECT * FROM warehouse`;
     
     connection.query(sqlCode, function(err, rows){
         res.send({
             "result": "ok",
-            "data": rows
+            "clothes": rows
         });
     });
 });
 
-app.get('/search/:brand', function (req, res) {
+app.get('/price-check', function (req, res) {
     let sqlCode = `SELECT * FROM licence_plates WHERE car_brand="${req.params.brand}"`;
     connection.query(sqlCode, function (err, row){
         res.send({
@@ -59,16 +57,16 @@ app.get('/search/:brand', function (req, res) {
     });
 });
 
-app.get('/add', function (req, res) {
-    // console.log(req.query.plate);
-    let sqlCode = `INSERT INTO licence_plates (plate, car_brand, car_model, color, year) VALUES ("${req.query.plate}", "${req.query.brand}", "${req.query.model}", "${req.query.color}", "${req.query.year}")`;
-    connection.query(sqlCode, function (err, row){
-        res.send({
-            "result": "ok",
-            "data": row
-        });
-    });
-});
+// app.get('/add', function (req, res) {
+//     // console.log(req.query.plate);
+//     let sqlCode = `INSERT INTO licence_plates (plate, car_brand, car_model, color, year) VALUES ("${req.query.plate}", "${req.query.brand}", "${req.query.model}", "${req.query.color}", "${req.query.year}")`;
+//     connection.query(sqlCode, function (err, row){
+//         res.send({
+//             "result": "ok",
+//             "data": row
+//         });
+//     });
+// });
 
 
 
