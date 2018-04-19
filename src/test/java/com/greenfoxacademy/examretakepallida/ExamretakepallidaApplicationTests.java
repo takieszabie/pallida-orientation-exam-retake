0c03af5091e902f085af3a278a7b77b358e3c15b
Szabolcs Takacs <szabolcs.tak@gmail.com>
2018-04-19 16:16:23 +0200

    project and database setup completed, screenshot saved under name dbshot.jpg

---
+++ src/test/java/com/greenfoxacademy/examretakepallida/ExamretakepallidaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy.examretakepallida;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExamretakepallidaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
