206e125493621b35f3a6b6492bd07214eec6d45a
Szabolcs Takacs <szabolcs.tak@gmail.com>
2018-04-19 19:15:49 +0200

    summary endpoint up and running

--- src/main/java/com/greenfoxacademy/examretakepallida/Controllers/MainController.java
+++ src/main/java/com/greenfoxacademy/examretakepallida/Controllers/MainController.java
@@ -1,18 +1,21 @@
 package com.greenfoxacademy.examretakepallida.Controllers;
 
+import com.greenfoxacademy.examretakepallida.Models.Item;
 import com.greenfoxacademy.examretakepallida.Repositories.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController {
 
   @Autowired
   ItemRepository itemRepository;
-
 
   @GetMapping("/warehouse")
   public String renderWarehouse(Model model){
@@ -22,9 +25,22 @@ public class MainController {
     return "warehouse";
   }
 
-  @PostMapping("/warehouse/summary")
-  public String renderSummary(){
-
-    return "summary";
+  @PostMapping(value = "/warehouse/summary")
+  public String summary(@ModelAttribute(name = "item") String itemName,
+                        @ModelAttribute(name = "size") String sizeNum,
+                        @ModelAttribute(name = "amount") int amount,
+                        Model model) {
+    List<Item> warehouses = itemRepository.findByItemNameAndSize(itemName, sizeNum);
+    if (warehouses.size() == 0) {
+      return "redirect:/warehouse";
+    } else {
+      model.addAttribute("itemName", itemName);
+      model.addAttribute("sizeNum", sizeNum);
+      model.addAttribute("amount", amount);
+      model.addAttribute("itemsList", warehouses);
+      Item item = warehouses.get(0);
+      model.addAttribute("subTotal", amount * item.getUnitPrice());
+      return "summary";
+    }
   }
 }
--- src/main/java/com/greenfoxacademy/examretakepallida/Repositories/ItemRepository.java
+++ src/main/java/com/greenfoxacademy/examretakepallida/Repositories/ItemRepository.java
@@ -3,6 +3,7 @@ package com.greenfoxacademy.examretakepallida.Repositories;
 import com.greenfoxacademy.examretakepallida.Models.Item;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -13,5 +14,8 @@ public interface ItemRepository extends CrudRepository<Item, Long> {
 
   @Query("select distinct c.size from Item c")
   List<String> findDistinctBySize();
+
+  @Query("select c from Item c where c.itemName like :itemName and c.size like :itemSize")
+  List<Item> findByItemNameAndSize(@Param("itemName") String itemNameInput, @Param("itemSize") String itemSizeInput);
 
 }
